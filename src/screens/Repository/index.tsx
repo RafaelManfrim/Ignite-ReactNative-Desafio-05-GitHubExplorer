a74aeef73f5686af191624a882bbdc9b4b93b5a0
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-05-04 21:05:47 -0300

    desafio concluido

--- src/screens/Repository/index.tsx
+++ src/screens/Repository/index.tsx
@@ -37,51 +37,33 @@ export function Repository() {
   const repository = findRepositoryById(repositoryId);
 
   function handleIssueNavigation(issueUrl: string) {
-    // TODO - use Linking to open issueUrl in a browser
+    Linking.openURL(issueUrl)
   }
 
   return (
     <Background>
       <Container>
         <RepoInfo>
-          {/* <OwnerAvatar source={{ uri:  }} /> */}
-
+          <OwnerAvatar source={{ uri: repository.owner.avatar_url }} />
           <TextGroup>
-            <TitleAnimation>
-              {
-                // TODO - full name of the repository
-              }
-            </TitleAnimation>
-
-            <Description numberOfLines={2}>{
-              //TODO - repository description
-            }</Description>
+            <TitleAnimation>{repository.full_name}</TitleAnimation>
+            <Description numberOfLines={2}>{repository.description}</Description>
           </TextGroup>
         </RepoInfo>
-
         <RepoStats>
           <Stars>
-            <StarsCounter>{
-              // TODO - repository stargazers count
-            }</StarsCounter>
+            <StarsCounter>{repository.stargazers_count}</StarsCounter>
             <StarsText>Stars</StarsText>
           </Stars>
-
           <Forks>
-            <ForksCounter>{
-              // TODO - repository forks count
-            }</ForksCounter>
+            <ForksCounter>{repository.forks_count}</ForksCounter>
             <ForksText>Forks</ForksText>
           </Forks>
-
           <OpenIssues>
-            <OpenIssuesCounter>{
-              // TODO - repository issues count
-            }</OpenIssuesCounter>
+            <OpenIssuesCounter>{repository.open_issues_count}</OpenIssuesCounter>
             <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
           </OpenIssues>
         </RepoStats>
-
         <IssuesList
           data={repository.issues}
           keyExtractor={issue => String(issue.id)}
@@ -93,7 +75,7 @@ export function Repository() {
                 title: issue.title,
                 subTitle: issue.user.login,
               }}
-            // TODO - onPress prop calling 
+              onPress={() => handleIssueNavigation(issue.html_url)}
             />
           )}
         />
